3dc2a0a344aaf11c34d20acaf29fbd5296dcd0d2
Daniel Tahin <tahin.daniel@gmail.com>
2020-04-08 13:40:06 +0200

    reset button and image added to drop animation

--- drop-animation/index.js
+++ drop-animation/index.js
@@ -1,9 +1,22 @@
 let drop = document.getElementById("drop")
 let shadow = document.getElementById("shadow")
 let reveal = document.getElementById("reveal")
+let content = document.getElementById("content")
+let reset = document.getElementById("reset")
 
 drop.addEventListener("click", () => {
 	drop.classList.add("dropped")
-	shadow.style.animation = "shadow 2s"
+	shadow.style.animation = "shadow 1s"
 	reveal.classList.add("revealed")
+	content.classList.add("flipped")
+})
+
+reset.addEventListener("click", () => {
+	drop.classList.remove("dropped")
+	shadow.style.webkitAnimation = "none"
+	setTimeout(() => {
+		shadow.style.webkitAnimation = '';
+	}, 10);
+	reveal.classList.remove("revealed")
+	content.classList.remove("flipped")
 })
